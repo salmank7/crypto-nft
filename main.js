e83f2d925b0abaf4338620b971bca8f67476bdc8
Salman Khan <salmank4954@gmail.com>
2023-07-04 11:39:54 -0700

    completed

--- main.js
+++ main.js
@@ -5,3 +5,34 @@ toggleBtn.addEventListener("click", (event) => {
   links.classList.toggle("top-[-10rem]");
   links.classList.toggle("top-12");
 });
+
+const questionElements = document.querySelectorAll(".faq__question");
+
+questionElements.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+
+    questionElements.forEach((otherQuestion) => {
+      const otherAnswer = otherQuestion.nextElementSibling;
+
+      if (otherQuestion !== question) {
+        otherQuestion.classList.remove("active");
+        otherAnswer.classList.remove("show");
+        otherQuestion
+          .querySelector("i")
+          .classList.replace("fa-minus", "fa-plus");
+      }
+    });
+
+    question.classList.toggle("active");
+    answer.classList.toggle("show");
+
+    const icon = question.querySelector("i");
+
+    if (icon.classList.contains("fa-plus")) {
+      icon.classList.replace("fa-plus", "fa-minus");
+    } else {
+      icon.classList.replace("fa-minus", "fa-plus");
+    }
+  });
+});
